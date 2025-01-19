ee86c3d904bf6d8caf9c45e5193b90459d0e69bd
Sayyid Shalahuddin <sayyidisal96@gmail.com>
2025-01-19 18:56:33 +0700

    API Limit UI Counter

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -14,6 +14,7 @@ import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FreeCounter } from "./free-counter";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -64,7 +65,13 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+};
+
+const Sidebar = ({
+  apiLimitCount = 0
+}: SidebarProps) => {
   const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -95,6 +102,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
